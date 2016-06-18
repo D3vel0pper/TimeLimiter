32660785fea5a3526481cdf2340094fc1d97917d
D3vel0pper <toamaindex@gmail.com>
2016-06-18 13:01:30 +0900

    add Activity

---
+++ app/src/main/java/d3vel0pper/com/timelimiter/activity/DatePickActivity.java
@@ -0,0 +1,15 @@
+package d3vel0pper.com.timelimiter.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by HotFlush on 2016/06/18.
+ */
+public class DatePickActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+}
--- app/src/main/java/d3vel0pper/com/timelimiter/fragment/DatePickerFragment.java
+++ app/src/main/java/d3vel0pper/com/timelimiter/fragment/DatePickerFragment.java
@@ -4,17 +4,36 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.Calendar;
 
+import d3vel0pper.com.timelimiter.R;
 import d3vel0pper.com.timelimiter.activity.MainActivity;
 
 /**
  * Created by HotFlush on 2016/06/16.
  */
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+
+    public DatePickerFragment(){
+    }
+
+    private DatePickerDialog dialog;
+
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        super.onCreateView(inflater,container,savedInstanceState);
+
+        View view = inflater.inflate(R.layout.fragment_date_picker,container,false);
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
@@ -22,7 +41,14 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
 
         MainActivity parent = (MainActivity) getActivity();
 
-        return new DatePickerDialog(getActivity(),parent,year,month,day);
+        dialog = new DatePickerDialog(getActivity(),parent,year,month,day);
+
+        return view;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        return dialog;
     }
 
     @Override
