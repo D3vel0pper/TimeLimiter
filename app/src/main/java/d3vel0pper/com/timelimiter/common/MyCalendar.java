673a505db0e081f08076b7979d40b19f3ce3f585
D3vel0pper <toamaindex@gmail.com>
2016-07-08 11:05:35 +0900

    fix day code to final

--- app/src/main/java/d3vel0pper/com/timelimiter/common/MyCalendar.java
+++ app/src/main/java/d3vel0pper/com/timelimiter/common/MyCalendar.java
@@ -35,13 +35,13 @@ public class MyCalendar {
     private String splited[];
     private int iTemp = 0;
     //day codes
-    public static int MONDAY = 0;
-    public static int TUESDAY = 1;
-    public static int WEDNESDAY = 2;
-    public static int THURSDAY = 3;
-    public static int FRIDAY = 4;
-    public static int SATURDAY = 5;
-    public static int SUNDAY = 6;
+    public static final int MONDAY = 0;
+    public static final int TUESDAY = 1;
+    public static final int WEDNESDAY = 2;
+    public static final int THURSDAY = 3;
+    public static final int FRIDAY = 4;
+    public static final int SATURDAY = 5;
+    public static final int SUNDAY = 6;
 
     //      methods
 
