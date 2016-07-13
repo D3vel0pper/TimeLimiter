135d42e8e2c20dfcbdb666d2afdd4457c4dfafa4
D3vel0pper <toamaindex@gmail.com>
2016-07-13 22:10:00 +0900

    setuped Edit Activity

--- app/src/main/java/d3vel0pper/com/timelimiter/activity/EditActivity.java
+++ app/src/main/java/d3vel0pper/com/timelimiter/activity/EditActivity.java
@@ -1,31 +1,42 @@
 package d3vel0pper.com.timelimiter.activity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import d3vel0pper.com.timelimiter.R;
 import d3vel0pper.com.timelimiter.common.DBData;
+import d3vel0pper.com.timelimiter.common.listener.ConfirmDialogListener;
 import d3vel0pper.com.timelimiter.common.listener.DialogTeller;
 import d3vel0pper.com.timelimiter.fragment.DatePickerFragment;
-import d3vel0pper.com.timelimiter.fragment.EditFragment;
+//import d3vel0pper.com.timelimiter.fragment.EditFragment;
 import d3vel0pper.com.timelimiter.fragment.TimePickerFragment;
 import io.realm.Realm;
 import io.realm.RealmQuery;
 import io.realm.RealmResults;
 import io.realm.Sort;
 
-public class EditActivity extends DatePickActivity {
+public class EditActivity extends DatePickActivity
+        implements ConfirmDialogListener,TextWatcher,DatePickerDialog.OnDateSetListener
+        ,TimePickerDialog.OnTimeSetListener {
     /**
      * [0] -> id  [1] -> createdAt  [2] -> title  [3] -> startDate  [4] -> endDate
      * [5] -> place  [6] -> description
@@ -72,6 +83,9 @@ public class EditActivity extends DatePickActivity {
         endTimeBtn.setOnClickListener(this);
         endBtn = (Button)findViewById(R.id.endBtn);
         endBtn.setOnClickListener(this);
+
+        dialogTeller = DialogTeller.getInstance();
+        dialogTeller.setListener(this);
     }
 
     @Override
@@ -129,6 +143,95 @@ public class EditActivity extends DatePickActivity {
 
     @Override
     public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+        if(this.TAG.equals("startDatePicker")){
+            dataList.set(3,String.valueOf(year) + "/"
+                    + String.valueOf(monthOfYear + 1) + "/" + String.valueOf(dayOfMonth)
+                    + " " + dataList.get(3).split(" ")[1]);
+            startDateText.setText(dataList.get(3));
+        } else if(this.TAG.equals("endDatePicker")){
+            dataList.set(4,String.valueOf(year) + "/"
+                    + String.valueOf(monthOfYear + 1) + "/" + String.valueOf(dayOfMonth)
+                    + " " + dataList.get(4).split(" ")[1]);
+            endDateText.setText(dataList.get(4));
+        }
+        //Put All Data
+        allData = titleText.getText() + "\n" + startGuide.getText() + "\n" + startText.getText() + "\n"
+                + endGuide.getText() + "\n" + endText.getText() + "\n"
+                + placeText.getText() + "\n" + descriptionText.getText();
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker,int hour, int min){
+        if(TAG.equals("startTimePicker")){
+            if(hour < 10){
+                if(min < 10){
+                    startTimeData = "0" + String.valueOf(hour) + ":" + "0" + String.valueOf(min);
+                }else {
+                    startTimeData = "0" + String.valueOf(hour) + ":" + String.valueOf(min);
+                }
+            }else {
+                if (min < 10) {
+                    startTimeData = String.valueOf(hour) + ":" + "0" + String.valueOf(min);
+                } else {
+                    startTimeData = String.valueOf(hour) + ":" + String.valueOf(min);
+                }
+            }
+            startDateText.setText(dataList.get(3).split(" ")[0] + " " + startTimeData);
+        } else if(TAG.equals("endTimePicker")){
+            if(hour < 10){
+                if(min < 10){
+                    endTimeData = "0" + String.valueOf(hour) + ":" + "0" + String.valueOf(min);
+                }else {
+                    endTimeData = "0" + String.valueOf(hour) + ":" + String.valueOf(min);
+                }
+            }else {
+                if (min < 10) {
+                    endTimeData = String.valueOf(hour) + ":" + "0" + String.valueOf(min);
+                } else {
+                    endTimeData = String.valueOf(hour) + ":" + String.valueOf(min);
+                }
+            }
+            endDateText.setText(dataList.get(4).split(" ")[0] + " " + endTimeData);
+        }
+        //Put All Data
+        allData = titleText.getText() + "\n" + startGuide.getText() + "\n" + startText.getText() + "\n"
+                + endGuide.getText() + "\n" + endText.getText() + "\n"
+                + placeText.getText() + "\n" + descriptionText.getText();
+    }
+
+    public String[] getTimeNow(){
+        /**
+         *       [0] -> date (yyyy/MM/dd)
+         *       [1] -> time (hh:mm)
+         */
+        //Set default time and date
+        String[] data;
+        String temp;
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.JAPAN);
+        temp = format.format(date);
+        data = temp.split(" ");
+        return data;
+    }
+
+    @Override
+    public void onConfirmDialog(String TAG){
+        this.TAG = TAG;
+    }
+
+    @Override
+    public void onTextChanged(CharSequence sequence,int start,int count,int after){
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence sequence,int start,int count,int after){
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable){
+        allData = titleText.getText() + "\n" + startGuide.getText() + "\n" + startText.getText() + "\n"
+                + endGuide.getText() + "\n" + endText.getText() + "\n"
+                + placeText.getText() + "\n" + descriptionText.getText();
     }
 
     }
--- app/src/main/java/d3vel0pper/com/timelimiter/fragment/EditFragment.java
+++ app/src/main/java/d3vel0pper/com/timelimiter/fragment/EditFragment.java
@@ -1,122 +1,122 @@
-package d3vel0pper.com.timelimiter.fragment;
-
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import d3vel0pper.com.timelimiter.R;
-import d3vel0pper.com.timelimiter.common.listener.ConfirmDialogListener;
-import d3vel0pper.com.timelimiter.common.listener.DialogTeller;
-
-/**
- * Created by HotFlush on 2016/07/12.
- */
-public class EditFragment extends Fragment
-        implements View.OnClickListener,ConfirmDialogListener{
-    /**
-     * [0] -> id  [1] -> createdAt  [2] -> title  [3] -> startDate  [4] -> endDate
-     * [5] -> place  [6] -> description
-     */
-    private List<String> dataList;
-    private TextView startDateText,endDateText;
-    private EditText titleText,placeText,descriptionText;
-    private Button startDateBtn,startTimeBtn,endDateBtn,endTimeBtn,endBtn;
-    private String TAG;
-    private DialogTeller dialogTeller;
-
-    private static EditFragment instance;
-
-    public static EditFragment getInstance(){
-        if(instance == null){
-            instance = new EditFragment();
-        }
-        return instance;
-    }
-
-    public void setDataList(List<String> list){
-        this.dataList = list;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        dialogTeller = DialogTeller.getInstance();
-        dialogTeller.setListener(this);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.activity_date_pick,container,false);
-        startDateText = (TextView)view.findViewById(R.id.startText);
-        startDateText.setText(dataList.get(3));
-        endDateText = (TextView)view.findViewById(R.id.endText);
-        endDateText.setText(dataList.get(4));
-        titleText = (EditText)view.findViewById(R.id.titleText);
-        titleText.setText(dataList.get(2));
-        placeText = (EditText)view.findViewById(R.id.placeText);
-        placeText.setText(dataList.get(5));
-        descriptionText = (EditText)view.findViewById(R.id.descriptionText);
-        descriptionText.setText(dataList.get(6));
-
-        startDateBtn = (Button)view.findViewById(R.id.startDateBtn);
-        startDateBtn.setOnClickListener(this);
-        startTimeBtn = (Button)view.findViewById(R.id.startTimeBtn);
-        startTimeBtn.setOnClickListener(this);
-        endDateBtn = (Button)view.findViewById(R.id.endDateBtn);
-        endDateBtn.setOnClickListener(this);
-        endTimeBtn = (Button)view.findViewById(R.id.endTimeBtn);
-        endTimeBtn.setOnClickListener(this);
-        endBtn = (Button)view.findViewById(R.id.endBtn);
-        endBtn.setOnClickListener(this);
-        return view;
-    }
-
-    @Override
-    public void onClick(View v){
-        switch(v.getId()){
-            case R.id.startDateBtn:
-                DatePickerFragment dpf = new DatePickerFragment();
-                dpf.show(getActivity().getSupportFragmentManager(),"startDatePicker");
-                break;
-            case R.id.startTimeBtn:
-                TimePickerFragment tpf = new TimePickerFragment();
-                tpf.show(getActivity().getSupportFragmentManager(),"startTimePicker");
-                break;
-            case R.id.endDateBtn:
-                DatePickerFragment pickerEndDate = new DatePickerFragment();
-                pickerEndDate.show(getActivity().getSupportFragmentManager(),"endDatePicker");
-                break;
-            case R.id.endTimeBtn:
-                TimePickerFragment pickerEndTime = new TimePickerFragment();
-                pickerEndTime.show(getActivity().getSupportFragmentManager(),"endTimePicker");
-                break;
-            case R.id.endBtn:
-                Toast.makeText(getActivity(), "ended correctly", Toast.LENGTH_SHORT).show();
-                getFragmentManager().beginTransaction().remove(this).commit();
-                getActivity().finish();
-                break;
-        }
-    }
-
-    @Override
-    public void onDestroyView(){
-        super.onDestroyView();
-    }
-
-        @Override
-    public void onConfirmDialog(String TAG){
-        this.TAG = TAG;
-    }
-
-}
+//package d3vel0pper.com.timelimiter.fragment;
+//
+//import android.content.SharedPreferences;
+//import android.os.Bundle;
+//import android.support.v4.app.Fragment;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.Button;
+//import android.widget.DatePicker;
+//import android.widget.EditText;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import d3vel0pper.com.timelimiter.R;
+//import d3vel0pper.com.timelimiter.common.listener.ConfirmDialogListener;
+//import d3vel0pper.com.timelimiter.common.listener.DialogTeller;
+//
+///**
+// * Created by HotFlush on 2016/07/12.
+// */
+//public class EditFragment extends Fragment
+//        implements View.OnClickListener,ConfirmDialogListener{
+//    /**
+//     * [0] -> id  [1] -> createdAt  [2] -> title  [3] -> startDate  [4] -> endDate
+//     * [5] -> place  [6] -> description
+//     */
+//    private List<String> dataList;
+//    private TextView startDateText,endDateText;
+//    private EditText titleText,placeText,descriptionText;
+//    private Button startDateBtn,startTimeBtn,endDateBtn,endTimeBtn,endBtn;
+//    private String TAG;
+//    private DialogTeller dialogTeller;
+//
+//    private static EditFragment instance;
+//
+//    public static EditFragment getInstance(){
+//        if(instance == null){
+//            instance = new EditFragment();
+//        }
+//        return instance;
+//    }
+//
+//    public void setDataList(List<String> list){
+//        this.dataList = list;
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState){
+//        super.onCreate(savedInstanceState);
+//        dialogTeller = DialogTeller.getInstance();
+//        dialogTeller.setListener(this);
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+//        View view = inflater.inflate(R.layout.activity_date_pick,container,false);
+//        startDateText = (TextView)view.findViewById(R.id.startText);
+//        startDateText.setText(dataList.get(3));
+//        endDateText = (TextView)view.findViewById(R.id.endText);
+//        endDateText.setText(dataList.get(4));
+//        titleText = (EditText)view.findViewById(R.id.titleText);
+//        titleText.setText(dataList.get(2));
+//        placeText = (EditText)view.findViewById(R.id.placeText);
+//        placeText.setText(dataList.get(5));
+//        descriptionText = (EditText)view.findViewById(R.id.descriptionText);
+//        descriptionText.setText(dataList.get(6));
+//
+//        startDateBtn = (Button)view.findViewById(R.id.startDateBtn);
+//        startDateBtn.setOnClickListener(this);
+//        startTimeBtn = (Button)view.findViewById(R.id.startTimeBtn);
+//        startTimeBtn.setOnClickListener(this);
+//        endDateBtn = (Button)view.findViewById(R.id.endDateBtn);
+//        endDateBtn.setOnClickListener(this);
+//        endTimeBtn = (Button)view.findViewById(R.id.endTimeBtn);
+//        endTimeBtn.setOnClickListener(this);
+//        endBtn = (Button)view.findViewById(R.id.endBtn);
+//        endBtn.setOnClickListener(this);
+//        return view;
+//    }
+//
+//    @Override
+//    public void onClick(View v){
+//        switch(v.getId()){
+//            case R.id.startDateBtn:
+//                DatePickerFragment dpf = new DatePickerFragment();
+//                dpf.show(getActivity().getSupportFragmentManager(),"startDatePicker");
+//                break;
+//            case R.id.startTimeBtn:
+//                TimePickerFragment tpf = new TimePickerFragment();
+//                tpf.show(getActivity().getSupportFragmentManager(),"startTimePicker");
+//                break;
+//            case R.id.endDateBtn:
+//                DatePickerFragment pickerEndDate = new DatePickerFragment();
+//                pickerEndDate.show(getActivity().getSupportFragmentManager(),"endDatePicker");
+//                break;
+//            case R.id.endTimeBtn:
+//                TimePickerFragment pickerEndTime = new TimePickerFragment();
+//                pickerEndTime.show(getActivity().getSupportFragmentManager(),"endTimePicker");
+//                break;
+//            case R.id.endBtn:
+//                Toast.makeText(getActivity(), "ended correctly", Toast.LENGTH_SHORT).show();
+//                getFragmentManager().beginTransaction().remove(this).commit();
+//                getActivity().finish();
+//                break;
+//        }
+//    }
+//
+//    @Override
+//    public void onDestroyView(){
+//        super.onDestroyView();
+//    }
+//
+//        @Override
+//    public void onConfirmDialog(String TAG){
+//        this.TAG = TAG;
+//    }
+//
+//}
