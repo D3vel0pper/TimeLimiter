4e45cd0fedcf8a8630d176e40e457d783f099774
D3vel0pper <toamaindex@gmail.com>
2016-07-23 17:19:43 +0900

    modified

--- app/src/main/java/d3vel0pper/com/timelimiter/fragment/CustomDialogFragment.java
+++ app/src/main/java/d3vel0pper/com/timelimiter/fragment/CustomDialogFragment.java
@@ -19,6 +19,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
